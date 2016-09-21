ce00a17cd9846be73476c8f61b036f63d9d3ae84
bianca c [arghgr] <tenmillionpeople@gmail.com>
2016-09-21 02:27:44 -0400

    Better labels

--- svc/data/getData.js
+++ svc/data/getData.js
@@ -17,24 +17,45 @@ function createDbQuery(query) {
     `WHERE ` +  filterQuery.join(`AND `) +
     `GROUP BY ` + sortBy + ` ` +
     `ORDER BY COUNT(` + sortBy + `) DESC;`;
-  return dbQuery;
+  var labelQuery = `SELECT * FROM ` + year + `labels_value WHERE field_name = '` + sortBy + `';`;
+  return { dbQuery: dbQuery, labelQuery: labelQuery };
 };
 
 exports.getData = (req, res, next)=> {
   if (DEBUG) console.log('exports.getData');
   var connection = null;
+  var dbResult = null;
+  var labelResult = null;
   try {
     if (DEBUG) console.log('req.query: ', req.query);
-    var dbQuery = createDbQuery(req.query);
+    var queries = createDbQuery(req.query);
+    var dbQuery = queries.dbQuery;
+    var labelQuery = queries.labelQuery;
     if (DEBUG) console.log('dbQuery: ', dbQuery);
+    if (DEBUG) console.log('labelQuery: ', labelQuery);
     db.getConnection()
     .then(conn => {
       connection = conn;
       return db.queryConnection(connection, dbQuery, req.params);
     })
-    .then(result => {
-      if (DEBUG) console.log('result: ', result);
-      res.send(result);
+    .then(result1 => {
+      dbResult = result1;
+      if (DEBUG) console.log('result1: ', result1);
+      return db.queryConnection(connection, labelQuery, req.params);
+    })
+    .then(result2 => {
+      labelResult = result2.reduce((map, obj) => {
+        map[obj.value] = obj.label.toString();
+        return map;
+      }, {});
+      if (DEBUG) console.log('labelResult: ', labelResult);
+      dbResult.forEach(result => {
+        result.value = result.name;
+        if (labelResult[result.name] !== undefined) result.name = labelResult[result.name];
+      });
+      if (DEBUG) console.log('dbResult: ', dbResult);
+
+      res.send(dbResult);
     })
     .catch(error => { throw error; });
   } catch (e) {
