210dc442dbe5c695454a2d88f40fdf0ff61e6532
bianca c [arghgr] <tenmillionpeople@gmail.com>
2016-09-29 01:12:20 -0400

    Add port env var for heroku deployment

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 app.use('/svc', svc);
 app.use(express.static('public'));
 
-app.listen(8080, ()=> console.log('Listening on port 8080.'));
+app.listen(process.env.PORT || 8080, ()=> console.log('Listening on port 8080.'));
 module.exports = app;
